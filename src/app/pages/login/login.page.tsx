1ba4c92835e8cd7c1f5c297ba95f19a0b7a8fb02
user <y.dubovitsky@gmail.com>
2023-01-24 12:40:16 +0300

    Login page has been stylized

--- src/app/pages/login/login.page.tsx
+++ src/app/pages/login/login.page.tsx
@@ -1,13 +1,24 @@
+import AtomicButtonComponent from "../../common/atomic-components/atomic-button/atomic-button.component";
 import styles from "./login.module.css";
 const Fade = require("react-reveal/Fade");
 
 const LoginPage: React.FC = () => {
   return (
     <div className={styles["container"]}>
-        <label htmlFor="username">Введите имя пользователя</label>
-        <input type="text" name="username" id="" placeholder="Администратор"/>
-        <label htmlFor="password">Введите ваш пароль</label>
-        <input type="password" name="password" id="" placeholder="123"/>
+      <Fade>
+        <div className={styles["login-form-container"]}>
+          <label htmlFor="username">Введите имя пользователя</label>
+          <input
+            type="text"
+            name="username"
+            id=""
+            placeholder="Администратор"
+          />
+          <label htmlFor="password">Введите ваш пароль</label>
+          <input type="password" name="password" id="" placeholder="123" />
+          <AtomicButtonComponent name="Войти" />
+        </div>
+      </Fade>
     </div>
   );
 };
