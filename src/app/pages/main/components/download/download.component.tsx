43b8c5ba4653fdf78477afa5e777e5d7b65fc0dc
y-dubovitsky <pXmJ5M9dvP>
2023-03-05 14:16:52 +0300

    Pictures have been changed in DownloadComponent on the MainPage

--- src/app/pages/main/components/download/download.component.tsx
+++ src/app/pages/main/components/download/download.component.tsx
@@ -1,13 +1,13 @@
-import styles from './download.module.css';
-import cn from 'classnames'
-import screen5 from './images/screen5.png';
+import styles from "./download.module.css";
+import cn from "classnames";
+import screen1 from "./images/screen1.png";
+import screen2 from "./images/screen2.png";
 import { Slide } from "react-awesome-reveal";
 
-const DownloadComponent : React.FC = () : JSX.Element => {
-
+const DownloadComponent: React.FC = (): JSX.Element => {
   return (
     <div id="explanation" className={styles["container"]}>
-      <Slide cascade={true} direction='right'>
+      <Slide cascade={true} direction="right">
         <div className={cn(styles["column"], styles["info"])}>
           <div className={styles["header"]}>
             <h1>"Почему стоит скачать наше приложение?"</h1>
@@ -18,18 +18,27 @@ const DownloadComponent : React.FC = () : JSX.Element => {
           <div className={styles["points"]}>
             <p className={styles["point"]}>Всегда под рукой</p>
             <p className={styles["point"]}>Приятный и функциональный дизайн</p>
-            <p className={styles["point"]}>Своевременное обновление меню на каждый день</p>
-            <p className={styles["point"]}>Регулярные обновления и обратная связь с разработчиком</p>
+            <p className={styles["point"]}>
+              Своевременное обновление меню на каждый день
+            </p>
+            <p className={styles["point"]}>
+              Регулярные обновления и обратная связь с разработчиком
+            </p>
           </div>
         </div>
       </Slide>
-      <Slide cascade={true} direction='left'>
+      <Slide cascade={true} direction="left">
         <div className={styles["column"]}>
-          <img className={styles["screen"]} src={screen5} alt=""/>
+          <img className={styles["screen"]} src={screen1} alt="" />
+        </div>
+      </Slide>
+      <Slide cascade={true} direction="left">
+        <div className={styles["column"]}>
+          <img className={styles["screen"]} src={screen2} alt="" />
         </div>
       </Slide>
     </div>
-  )
-}
+  );
+};
 
 export default DownloadComponent;
