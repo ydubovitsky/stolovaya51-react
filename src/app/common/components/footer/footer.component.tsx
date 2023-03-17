5fcfda4e7fd7966254f90d2820563a8ab85bcd52
user <y.dubovitsky@gmail.com>
2023-03-17 13:02:00 +0300

    new links added to the footer

--- src/app/common/components/footer/footer.component.tsx
+++ src/app/common/components/footer/footer.component.tsx
@@ -3,17 +3,25 @@ import imageSrc from "./images/jimmy-dean-Yn0l7uwBrpw-unsplash.jpg";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMapMarkedAlt } from "@fortawesome/free-solid-svg-icons";
+import { REACT_APP_DOWNLOAD_ANDROID_APP_LINK } from "../../../../constants";
 
-const FooterComponent = () => {
+const FooterComponent: React.FC = (): JSX.Element => {
   return (
     <div className={styles["container"]}>
       <div className={styles["about-container"]}>
         <div className={styles["logo-container"]}>
           <div className={styles["logo"]}>Столовая51 в НИИ ТП</div>
           <div className={styles["anchors-container"]}>
-            <p><Link to={"/"}>Главная страница |</Link></p>
-            <p><Link to={"/privacy-policy"}>Политика конфеденциальности |</Link></p>
-            <p>Скачать наше приложение</p>
+            <p>
+              <Link to={"/"}>Главная страница |</Link>
+            </p>
+            <p>
+              <Link to={"/term-of-use"}>Пользовательское соглашение |</Link>
+            </p>
+            <p>
+              <Link to={"/privacy-policy"}>Политика конфеденциальности |</Link>
+            </p>
+            <p><Link target="_blank" to={REACT_APP_DOWNLOAD_ANDROID_APP_LINK}>Скачать приложение |</Link></p>
           </div>
         </div>
         <div className={styles["contacts-container"]}>
@@ -30,7 +38,7 @@ const FooterComponent = () => {
       ></div>
       <div className={styles["author-container"]}>
         <p>
-          © 2023 - {new Date().getFullYear()} All Rights Reserved. Created By 
+          © 2023 - {new Date().getFullYear()} All Rights Reserved. Created By
           <a href="http://ydubovitsky.ru"> Y.Dubovitsky</a>
         </p>
       </div>
---
+++ src/constants/index.ts
@@ -0,0 +1,1 @@
+export const REACT_APP_DOWNLOAD_ANDROID_APP_LINK : string = "https://appgallery.huawei.com/app/C107809409?sharePrepath=ag&locale=ru_RU&source=appshare&subsource=C107809409&shareTo=com.android.bluetooth&shareFrom=appmarket&shareIds=6103fad53227440598de601a249fe34b_com.android.bluetooth&callType=SHARE";
