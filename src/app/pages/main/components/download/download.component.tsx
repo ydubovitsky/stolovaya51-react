3fa5c4ead8ffe16840de5b1928d6efaee6339177
user <y.dubovitsky@gmail.com>
2023-03-17 13:36:36 +0300

    qr code added for downloading app

--- src/app/pages/main/components/download/download.component.tsx
+++ src/app/pages/main/components/download/download.component.tsx
@@ -1,8 +1,12 @@
 import styles from "./download.module.css";
+import { Link } from "react-router-dom";
 import cn from "classnames";
 import screen1 from "./images/screen1.png";
 import screen2 from "./images/screen2.png";
+import qrcode from "./images/qrcode.png";
 import { Slide } from "react-awesome-reveal";
+import { REACT_APP_DOWNLOAD_ANDROID_APP_LINK } from "../../../../../constants";
+import AtomicButtonComponent from "../../../../common/atomic-components/atomic-button/atomic-button.component";
 
 const DownloadComponent: React.FC = (): JSX.Element => {
   return (
@@ -35,6 +39,26 @@ const DownloadComponent: React.FC = (): JSX.Element => {
       <Slide cascade={true} direction="left">
         <div className={styles["column"]}>
           <img className={styles["screen"]} src={screen2} alt="" />
+        </div>
+      </Slide>
+      <Slide cascade={true} direction="left">
+        <div className={cn(styles["column"], styles["qrcode"])}>
+          <h2>
+            Наведите камеру вашего мобильного телефона на код и скачайте наше
+            приложение
+          </h2>
+          <img className={styles["screen"]} src={qrcode} alt="" />
+          <p>
+            или просто нажмите на кнопку ниже (временно доступна только андроид
+            версия)...
+          </p>
+          <Link
+            className={styles["download-button"]}
+            target="_blank"
+            to={REACT_APP_DOWNLOAD_ANDROID_APP_LINK}
+          >
+            <AtomicButtonComponent name="Скачать приложение" />
+          </Link>
         </div>
       </Slide>
     </div>
