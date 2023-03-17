d1412516829d614359ecbb0fa189703b49396094
user <y.dubovitsky@gmail.com>
2023-03-17 13:01:30 +0300

    Term of use added; privacy-policy updated

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -7,12 +7,14 @@ import MealSubpage from "../pages/dashboard/subpages/meal/meal.subpage";
 import MenuSubpage from "../pages/dashboard/subpages/menu/menu.subpage";
 import RequireAuthRoute from "../../hoc/require-auth-route.hoc";
 import PrivacyPolicyPage from "../pages/privacy-policy/privacy-policy.page";
+import TermOfUsePage from "../pages/term-of-use/term-of-use.page";
 
 const RoutesComponent = () => (
   <Routes>
     <Route path="/" element={<MainLayout />}>
       <Route index element={<MainPage />} />
       <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+      <Route path="/term-of-use" element={<TermOfUsePage />} />
       <Route element={<RequireAuthRoute />}>
         <Route path="login" element={<LoginPage />} />
         <Route path="dashboard" element={<DashboardPage />}>
